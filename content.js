199738f91ff109d4201c30e8e4c01a24d2c114b3
andretan <andretan@stud.ntnu.no>
2024-08-22 02:43:36 +0200

    made popup for extension

--- content.js
+++ content.js
@@ -1,6 +1,10 @@
-let div = document.getElementById('board-layout-main');
-
-if (div) {
-  div.style.justifyContent = "center";
-  div.style.paddingBottom = "12rem"
-}
+chrome.runtime.onMessage.addListener(function(request, _sender, _sendResponse) {
+    if (request.action === "toggleCenter") {
+        console.log("test")
+        const boardDiv = document.getElementById('board-layout-main');
+        if (boardDiv) {
+            const isCentered = boardDiv.style.justifyContent === 'center';
+            boardDiv.style.justifyContent = isCentered ? '' : 'center';
+        }
+    }
+});
