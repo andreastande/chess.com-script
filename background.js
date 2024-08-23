e23ec57c13cb82fe9e5c5c68229c153048a30099
andretan <andretan@stud.ntnu.no>
2024-08-23 04:25:47 +0200

    now it finally works

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
     // Check if the URL matches the pattern and inject the appropriate script
-    if (details.url.startsWith("https://www.chess.com/game/live/")) {
+    if (details.url.startsWith("https://www.chess.com/game/")) {
         chrome.scripting.executeScript({
             target: { tabId: details.tabId },
             files: ['playerInfo.js']
