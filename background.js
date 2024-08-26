e91daa15dc11d2f1463475b2d8d186aaa1163c2a
andretan <andretan@stud.ntnu.no>
2024-08-26 23:24:18 +0200

    quick test

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function (details) {
     // Check if the URL matches the pattern and inject the appropriate script
-    if (details.url.startsWith("https://www.chess.com/game/")) {
+    if (details.url.startsWith("https://www.chess.com/game/") || details.url.startsWith("https://www.chess.com/game/live/")) {
         chrome.scripting.executeScript({
             target: { tabId: details.tabId },
             files: ['playerInfo.js']
